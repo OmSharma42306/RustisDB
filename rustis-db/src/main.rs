6f361ff472627035740f9e9ae73f2b61d134d38e
Om Sharma <omsharma.83173@gmail.com>
2025-09-17 20:26:26 +0530

    feat : added-hashmap-storage-SET/GET-methods.

--- rustis-db/src/main.rs
+++ rustis-db/src/main.rs
@@ -1,10 +1,14 @@
 use std::io::{Read,Write};
 use std::net::{TcpListener,TcpStream};
 use std::thread;
+use std::collections::HashMap;
+use std::sync::{Arc,Mutex};
 
 
+type Db = Arc<Mutex<HashMap<String,String>>>;
 
-fn handle_client(mut stream : TcpStream){
+
+fn handle_client(mut stream : TcpStream,db : Db){
     let mut buffer = [0;512];
 
     loop{
@@ -15,8 +19,29 @@ fn handle_client(mut stream : TcpStream){
                 break;
             }
             Ok(n)=>{
-                println!("Received : {:?}",String::from_utf8_lossy(&buffer[..n]));
-                stream.write_all(b"+PONG\r\n").unwrap(); 
+                // println!("Received : {:?}",String::from_utf8_lossy(&buffer[..n]));
+                // stream.write_all(b"+PONG\r\n").unwrap(); 
+
+                let input = String::from_utf8_lossy(&buffer[..n]);
+                let parts : Vec<&str> = input.trim().split_whitespace().collect();
+
+                let response = match parts.as_slice(){
+                    ["P"] => "+PONG\r\n".to_string(),
+                    ["SET",key,value] =>{
+                        db.lock().unwrap().insert(key.to_string(), value.to_string()); 
+                        "+OK\r\n".to_string()
+                    }
+                    ["GET",key] =>{
+                        if let Some(val) = db.lock().unwrap().get(*key){
+                            format!("${}\r\n{}\r\n",val.len(),val)
+                        }else{
+                            "$-1\r\n".to_string() // Null Bulk String return.
+                        }
+                    }
+                    _=> "-ERR unknown command\r\n".to_string(),
+                };
+
+                stream.write_all(response.as_bytes()).unwrap();
             }
             Err(e)=>{
                 eprintln!("Error : {}",e);
@@ -30,13 +55,13 @@ fn main(){
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     println!("Mini Redis by Om Sharma Running at PORT : 6379..");
 
+    let db : Db = Arc::new(Mutex::new(HashMap::new()));
+
     for stream in listener.incoming(){
-        match stream{
-            Ok(stream)=>{
+                // create an pointer to an copy of db;
+                let db = Arc::clone(&db);
                 // spawn thread.
-                thread::spawn(|| handle_client(stream));
-            }
-            Err(e)=> eprintln!("Connection Failed! {}",e),
-        }
+                thread::spawn(move || handle_client(stream.unwrap(),db));
+
     }
 }
